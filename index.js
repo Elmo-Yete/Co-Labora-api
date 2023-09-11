932971cc2c7ea57b48b7c3157dff8c415f6fbb56
DamianT <elmo@DESKTOP-OP1RDAI>
2023-09-11 02:20:48 -0600

    add: working tree cinection to mongodb

---
+++ index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const { mongoose } = require("mongoose");
+const express = require("express");
+const app = express();
+const { DB_USERNAME, DB_PASSWORD, DB_URL, DB_NAME } = process.env;
+const dataBaseUrl = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_URL}/${DB_NAME}`;
+const port = process.env.PORT || 8080;
+
+mongoose
+  .connect(dataBaseUrl)
+  .then(() => {
+    console.log("Conexion a la base de datos exitosa");
+    app.listen(port, () => {
+      console.log("Servidor levantado");
+    });
+  })
+  .catch((err) => {
+    console.log("no se logro conectar con la base de datos", err);
+  });
