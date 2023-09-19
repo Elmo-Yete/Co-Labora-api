053ebcf2bc5059da79953c78831884dceb684578
Manuel Cabrera <josemanuelcabrera2011@gmail.com>
2023-09-18 20:34:46 -0600

    add: models for API & change existing models for correct types

--- src/models/reservation.model.js
+++ src/models/reservation.model.js
@@ -2,41 +2,98 @@ const mongoose = require("mongoose");
 
 const reservationSchema = new mongoose.Schema({
   _id: {
-    type: Number,
-  },
-  property_id: {
-    type: Number,
-  },
-  initial_date: {
-    type: Number,
-  },
-  final_date: {
-    type: Number,
-  },
-  lessor: {
     type: String,
   },
-  tenant: {
-    type: String,
+  property: {
+    propetyId: {
+      type: String,
+      require: true,
+    },
+    propertyName: {
+      type: String,
+      require: true
+    },
+    score: {
+      type: Number,
+      require: true
+    },
+    propertyImage: {
+      type: String,
+      require: true
+    }
   },
-  add_ons: {
-    type: Array,
+  startDate: {
+    type: String,
+    require: true,
+  },
+  endDate: {
+    type: String,
+    require: true,
+  },
+  lessorId: {
+    type: String,
+    require: true,
+  },
+  tenantId: {
+    type: String,
+    require: true,
+  },
+  addOns: {
+    screwdrivers: {
+      type: Boolean,
+      require: true,
+    },
+    powerExtension: {
+      type: Boolean,
+      require: true,
+    },
+    flexometer: {
+      type: Boolean,
+      require: true,
+    },
+    drill: {
+      type: Boolean,
+      require: true,
+    },
+    carpenterBrush: {
+      type: Boolean,
+      require: true,
+    },
+    woodJigSaw: {
+      type: Boolean,
+      require: true,
+    },
   },
   subtotal: {
     type: Number,
+    require: true,
   },
-  commition: {
+  commission: {
     type: Number,
+    require: true,
   },
   taxes: {
     type: Number,
+    require: true,
   },
   total: {
     type: Number,
+    require: true,
   },
-  payment_intent: {
-    type: String,
+  paymentIntent: {
+    cardNumber: {
+      type: Number,
+      require: true,
+    },
+    cardExpirationDate: {
+      type: String,
+      require: true
+    }
   },
+  termsAndConditionsDeal: {
+    type: Boolean,
+    require: true
+  }
 });
 
 module.exports = mongoose.model(
