b3a28e6d36d980a1ccdc185a59e123dde684be49
DamianT <elmo@DESKTOP-OP1RDAI>
2023-09-19 15:22:03 -0600

    add: route login createUser deleteUser rating comments

--- src/routes/comments.route.js
+++ src/routes/comments.route.js
@@ -1,5 +1,38 @@
 const express = require("express");
 const router = express.Router();
+const { getComments, deleteComment } = require("../usecases/comments.usecases");
 
+router.get("/comments", async (req, res) => {
+  try {
+    const comment = await getComments();
+    res.status(201);
+    res.json({
+      success: true,
+      data: comment,
+    });
+  } catch (err) {
+    res.status(err.status || 500);
+    res.json({
+      succes: false,
+      message: err.message,
+    });
+  }
+});
+
+router.delete("/deleteComment", async (req, res) => {
+  try {
+    const comment = await deleteComment();
+    res.status(201);
+    res.json({
+      success: true,
+    });
+  } catch (err) {
+    res.status(err.status || 500);
+    res.json({
+      succes: false,
+      message: err.message,
+    });
+  }
+});
 
 module.exports = router;
