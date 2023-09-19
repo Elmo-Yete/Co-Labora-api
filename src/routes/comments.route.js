5620b516c6ffeef041875d3826f5d16aff983905
Manuel Cabrera <josemanuelcabrera2011@gmail.com>
2023-09-19 00:00:37 -0600

    add: favorites route

--- src/routes/comments.route.js
+++ src/routes/comments.route.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const { create, extract } = require("../usecases/user.usecase");
+
 
 module.exports = router;
--- src/routes/datesNotAvalible.route.js
+++ src/routes/datesNotAvalible.route.js
@@ -1,5 +1,4 @@
 const express = require("express");
 const router = express.Router();
-const { create, extract } = require("../usecases/user.usecase");
 
 module.exports = router;
