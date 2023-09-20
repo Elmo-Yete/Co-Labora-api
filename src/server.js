563da2e2293bb540c4c08a1360e575a6fb65fc55
Manuel Cabrera <josemanuelcabrera2011@gmail.com>
2023-09-19 18:48:43 -0600

    add: add dates and get dates

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const routerLogin = require("./routes/auth.route")
 const routerUser = require("./routes/user.route");
+const routerDatesNotAvailable = require("./routes/datesNotAvailable.route")
 const cors = require("cors");
 
 const app = express();
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/", routerUser);
-app.use("/login", routerLogin)
+app.use("/login", routerLogin);
+app.use("/datesNotAvailable");
 
 module.exports = app;
