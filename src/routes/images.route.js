52744d32988761858bd488d3f88a97e8f1737b6b
Manuel Cabrera <josemanuelcabrera2011@gmail.com>
2023-10-16 22:19:21 -0600

    add images to propety at save point

--- src/routes/images.route.js
+++ src/routes/images.route.js
@@ -3,17 +3,35 @@ const router = express.Router();
 const { saveImages, deletedImage } = require("../usecases/images.usecases");
 const auth = require("../middlewares/auth.middleware");
 const upload = require("../middlewares/awsS3.middleware");
-const arrayUpload = upload.array("images", 10);
+const arrayUpload = upload.fields([
+  {name: "property-images", maxCount: 10}, 
+  {name: "property-documents", maxCount: 10},
+  {name: "property-dni", maxCount: 10}
+]);
+
 
 router.post("/upload", auth,  arrayUpload,  async (req, res) => {
   try{
-    const files = req.files;
-    const updateFilesInfo = files.map(file => file.location
+    const filesImages = req.files["property-images"];
+    const filesDocs = req.files["property-documents"];
+    const filesDni = req.files["property-dni"];
+    const propertyImages = filesImages.map(file => file.location
     );
+    const propertyDocs = filesDocs.map(file => file.location
+    );
+    const propertyDni = filesDni.map(file => file.location
+    );
+    const output = {
+      imagesProperty: propertyImages,
+      docsProperty: propertyDocs,
+      dniProperty: propertyDni,
+      name: req.body.name,
+      location: req.body.location
+    }
     res.status(201)
     res.json({
       success: true,
-      data: updateFilesInfo
+      data: output
     })
   } catch(err){
     res.status(err.status || 500)
--- src/routes/property.route.js
+++ src/routes/property.route.js
@@ -8,10 +8,72 @@ const {
   patchProperty,
 } = require("../usecases/property.usecases");
 const auth = require("../middlewares/auth.middleware");
+const upload = require("../middlewares/awsS3.middleware");
+const arrayUpload = upload.fields([
+  {name: "property-images", maxCount: 10}, 
+  {name: "property-documents", maxCount: 10},
+  {name: "property-dni", maxCount: 10}
+]);
 
-router.post("/", auth, async (req, res) => {
+router.post("/", auth, arrayUpload, async (req, res) => {
   try {
-    const user = await createProperty(req.body);
+    const filesImages = req.files["property-images"];
+    const filesDocs = req.files["property-documents"];
+    const filesDni = req.files["property-dni"];
+    const propertyImages = filesImages.map(file => file.location
+    );
+    const propertyDocs = filesDocs.map(file => file.location
+    );
+    const propertyDni = filesDni.map(file => file.location
+    );
+    const propertyData = {
+      imagesProperty: propertyImages,
+      docsProperty: propertyDocs,
+      dniProperty: propertyDni,
+      userId: req.body.userId,
+      name: req.body.name,
+      location: {
+        ownerLocationDescription: req.body.ownerLocationDescription,
+        street: req.body.street,
+        neighbor: req.body.neighbor,
+        number: req.body.number,
+        zip: req.body.zip,
+        city: req.body.city,
+        mapCoordinates: {
+          lat: req.body.lat,
+          lng: req.body.lng
+        }
+      },
+      description: req.body.description,
+      amenities: {
+        wifi: req.body.wifi,
+        parking: req.body.parking,
+        airConditioner: req.body.airConditioner,
+        reception: req.body.reception,
+        petFriendly: req.body.petFriendly,
+        cleanServices: req.body.cleanServices
+      },
+      addOns: {
+        screwdrivers: req.body.screwdrivers,
+        powerExtension: req.body.powerExtension,
+        flexometer: req.body.flexometer,
+        drill: req.body.drill,
+        carpenterBrush: req.body.carpenterBrush,
+        woodJigSaw: req.body.woodJigSaw
+      },
+      price: req.body.price, 
+      ownerName: req.body.ownerName,
+      measurements: {
+        long: req.body.long,
+        with: req.body.width,
+      },
+      workTime: {
+        open: req.body.open,
+        close: req.body.close  
+      }
+    }
+    console.log("property", propertyData)
+    const user = await createProperty(propertyData);
     res.status(201);
     res.json({
       success: true,
